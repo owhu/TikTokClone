87865210a3e359aa74131a08d41fb23d22320e7d
oliver <oliverhu.oh@gmail.com>
2024-08-13 13:52:05 -0700

    Initial Commit

---
+++ TikTokClone/App/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Oliver Hu on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
