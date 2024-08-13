8e24aee10a091d0088074e9621ea10d16883ed62
oliver <oliverhu.oh@gmail.com>
2024-08-13 14:23:26 -0700

    project updated - added tab views

---
+++ TikTokClone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  TikTokClone
+//
+//  Created by Oliver Hu on 8/13/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
