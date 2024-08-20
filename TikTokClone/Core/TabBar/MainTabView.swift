400a14572c2be9f851e68f1b1f9e3d5fd6f83aa1
oliver <oliverhu.oh@gmail.com>
2024-08-20 15:06:49 -0700

    project updated - updated FeedCellView

--- TikTokClone/Core/TabBar/MainTabView.swift
+++ TikTokClone/Core/TabBar/MainTabView.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct MainTabView: View {
+    @State private var selectedTab = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView(selection: $selectedTab) {
+            FeedView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "house")
+                            .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
+                        Text("Home")
+                    }
+                }
+                .onAppear { selectedTab = 0 }
+                .tag(0)
+            
+            Text("Friends")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "person.2")
+                            .environment(\.symbolVariants, selectedTab == 1 ? .fill : .none)
+                        Text("Friends")
+                    }
+                }
+                .onAppear { selectedTab = 1 }
+                .tag(1)
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus")
+                        .environment(\.symbolVariants, selectedTab == 2 ? .fill : .none)
+                }
+            
+            Text("Notifications")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "heart")
+                            .environment(\.symbolVariants, selectedTab == 3 ? .fill : .none)
+                        Text("Inbox")
+                    }
+                }
+                .onAppear { selectedTab = 3 }
+                .tag(3)
+            
+            Text("Profile")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "person")
+                            .environment(\.symbolVariants, selectedTab == 4 ? .fill : .none)
+                        Text("Profile")
+                    }
+                }
+                .onAppear { selectedTab = 4 }
+                .tag(4)
+        }
+        .tint(.black)
     }
 }
 
