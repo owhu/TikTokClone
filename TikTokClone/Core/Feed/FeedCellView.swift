679fe413974b7c27e3219eaa9eb084321295dd79
oliver <oliverhu.oh@gmail.com>
2024-08-20 14:40:58 -0700

    project updated - Feed and FeedCell View added

---
+++ TikTokClone/Core/Feed/FeedCellView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCellView.swift
+//  TikTokClone
+//
+//  Created by Oliver Hu on 8/20/24.
+//
+
+import SwiftUI
+
+struct FeedCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedCellView()
+}
---
+++ TikTokClone/Core/Feed/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  TikTokClone
+//
+//  Created by Oliver Hu on 8/20/24.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
