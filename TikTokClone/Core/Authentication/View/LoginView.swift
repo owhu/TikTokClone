1ee43a4a9d1120ed53e53e8c559fe08e152a03b9
oliver <oliverhu.oh@gmail.com>
2024-08-27 13:49:54 -0700

    project updated - login view updated

--- TikTokClone/Core/Authentication/View/LoginView.swift
+++ TikTokClone/Core/Authentication/View/LoginView.swift
@@ -8,8 +8,79 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                // logo image
+                Image("tiktok-app-icon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120, height: 120)
+                    .padding()
+                
+                // textfields
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .textInputAutocapitalization(.never)
+                        .modifier(StandardTextfieldModifier())
+                    
+                    TextField("Enter your password", text: $password)
+                        .textInputAutocapitalization(.never)
+                        .modifier(StandardTextfieldModifier())
+                }
+                
+                NavigationLink {
+                    Text("Forgot password")
+                } label: {
+                    Text("Forgot password")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+                
+                // login button
+                
+                Button {
+                    print("DEBUG: Login")
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .frame(width: 350, height: 44)
+                        .background(.pink)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+                .padding(.vertical)
+                .disabled(email.isEmpty && password.isEmpty)
+                
+                Spacer()
+                
+                // go to sign up
+                Divider()
+                
+                NavigationLink {
+                    RegistrationView()
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                    .padding(.vertical)
+                }
+
+            }
+        }
     }
 }
 
--- TikTokClone/Utilities/StandardTextfieldModifier.swift
+++ TikTokClone/Utilities/StandardTextfieldModifier.swift
@@ -5,4 +5,15 @@
 //  Created by Oliver Hu on 8/27/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct StandardTextfieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .padding(12)
+            .background(Color(.systemGray6))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .padding(.horizontal, 24)
+    }
+}
